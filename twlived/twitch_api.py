b6f5a9e384781748c5c74ec71fe26f38ab79fb4a
Kirill Golenitskii <sterk12@gmail.com>
2018-03-03 13:01:55 +0300

    update TwitchAPI: live playlists, webhooks end-point

--- twlived/twitch_api.py
+++ twlived/twitch_api.py
@@ -1,18 +1,18 @@
 import functools
 import json
 from itertools import chain
-from time import time as utc
-from typing import Dict, List, Callable, Tuple, Any, Optional, Union
+from time import time as utc, time
+from typing import Dict, List, Callable, Tuple, Any, Optional, Union, TypeVar
+from urllib.parse import urljoin
 
 import requests
-from mypy_extensions import DefaultNamedArg
 
 from .config_logging import log
 
 log = log.getChild('TwitchAPI')
 
-RF = Callable[[str, DefaultNamedArg(Optional[Dict], 'params'),
-               DefaultNamedArg(Optional[Dict[str, str]], 'headers')], requests.Response]
+RF = Callable[..., requests.Response]
+T = TypeVar('T')
 
 
 def timed_cache(func: Callable[..., Dict]) -> Callable[..., Dict]:
@@ -33,6 +33,10 @@ def filter_none_and_empty(dictionary: Dict) -> Dict:
     return {key: value for key, value in dictionary.items() if value}
 
 
+def identity(obj: T) -> T:
+    return obj
+
+
 class TwitchAPI:
     """Class implementing part of Twitch API Helix."""
 
@@ -44,13 +48,18 @@ class TwitchAPI:
     PERIODS = {'all', 'day', 'month', 'week'}
     SORT_VALUES = {'time', 'trending', 'views'}
     VIDEO_TYPES = {'all', 'upload', 'archive', 'highlight'}
+    HUB_MODES = {'subscribe', 'unsubscribe'}
     headers: Dict[str, str] = {'Content-Type': 'application/json'}
 
-    def __init__(self, client_id: str, *, request_wrapper: Optional[Callable[[RF], RF]] = None) -> None:
+    def __init__(self, client_id: str, *, request_wrapper: Callable[[RF], RF] = identity) -> None:
         self.headers.update({'Client-ID': client_id})
-        self._request_wrapper = request_wrapper
+        self._wrap_requests(request_wrapper)
         self._id_storage: Dict[str, Dict] = {}
         self._login_storage: Dict[str, Dict] = {}
+        self._session = requests.Session()
+        self._session.headers.update(self.headers)
+        self._ratelimit_remaining = 30
+        self._ratelimit_reset: Union[int, float] = time()
 
     # noinspection PyShadowingBuiltins
     def get_streams(self, *,
@@ -90,7 +99,7 @@ class TwitchAPI:
             'user_id': user_id,
             'user_login': user_login,
         })
-        response = self._helix_get('streams', params=params).json()
+        response = self._helix_get('streams', params=params)
         return response['data'], response['pagination']['cursor'] if response['pagination'] else None
 
     # noinspection PyShadowingBuiltins
@@ -134,7 +143,7 @@ class TwitchAPI:
             'sort': sort,
             'type': type,
         })
-        response = self._helix_get('videos', params=params).json()
+        response = self._helix_get('videos', params=params)
         return response['data'], response['pagination']['cursor'] if response['pagination'] else None
 
     # noinspection PyShadowingBuiltins
@@ -159,7 +168,7 @@ class TwitchAPI:
             'login': missing_logins,
         })
         if params:
-            response = self._helix_get('users', params=params).json()
+            response = self._helix_get('users', params=params)
             for user in response['data']:
                 self._id_storage[user['id']] = user
                 self._login_storage[user['login']] = user
@@ -185,24 +194,82 @@ class TwitchAPI:
                              'allow_audio_only': 'true',
                          }).text
 
-    @staticmethod
-    def __get(url: str, *,
-              params: Optional[Dict] = None,
-              headers: Optional[Dict[str, str]] = None) -> requests.Response:
-        response = requests.get(url, params=params, headers=headers)
-        response.raise_for_status()
-        # TODO: support Rate Limits https://dev.twitch.tv/docs/api#rate-limits
+    def get_channel_token(self, login: str) -> Dict:
+        response: Dict = self._get(f'{TwitchAPI.TOKEN_DOMAIN}channels/{login}/access_token',
+                                   params={'need_https': 'true'},
+                                   headers=self.headers).json()
         return response
 
-    def _get(self, url: str, *,
+    def get_live_variant_playlist(self, login: str) -> str:
+        token = self.get_channel_token(login)
+        return self._get(f'{TwitchAPI.USHER_DOMAIN}api/channel/hls/{login}.m3u8',
+                         params={'token': token['token'],
+                                 'sig': token['sig'],
+                                 'allow_source': 'true'}).text
+
+    def post_webhook(self, hub_callback: str, hub_mode: str, hub_topic: 'HubTopic', *,
+                     hub_lease_seconds: int = 864000,
+                     hub_secret: str = '') -> None:
+        if hub_mode not in TwitchAPI.HUB_MODES:
+            raise ValueError(f'Invalid hub.mode. Valid values: {TwitchAPI.HUB_MODES}')
+        params = {
+            'hub.callback': hub_callback,
+            'hub.mode': hub_mode,
+            'hub.topic': hub_topic,
+            'hub.lease_seconds': str(hub_lease_seconds),
+            'hub.secret': hub_secret,
+        }
+        self._helix_post('webhooks/hub', params=params)
+
+    def _request(self, method: str, url: str, *,
+                 params: Optional[Dict] = None) -> requests.Response:
+        if not self._ratelimit_remaining and self._ratelimit_reset > time():
+            raise RateLimitOverflow(f'Wait {self._ratelimit_reset} until the limit is reset')
+        response = self._session.request(method, url, params=params)
+        ratelimit_remaining = response.headers.get('Ratelimit-Remaining', None)
+        ratelimit_reset = response.headers.get('Ratelimit-Reset', None)
+        self._ratelimit_remaining = int(ratelimit_remaining) if ratelimit_remaining else self._ratelimit_remaining
+        self._ratelimit_reset = int(ratelimit_reset) if ratelimit_reset else self._ratelimit_reset
+        response.raise_for_status()
+        return response
+
+    @staticmethod
+    def _get(url: str, *,
              params: Optional[Dict] = None,
              headers: Optional[Dict[str, str]] = None) -> requests.Response:
-        if self._request_wrapper is not None:
-            get_url = self._request_wrapper(self.__get)
-        else:
-            get_url = self.__get
-        response = get_url(url, params=params, headers=headers)
+        response = requests.get(url, params=params, headers=headers)
+        response.raise_for_status()
         return response
 
-    def _helix_get(self, path: str, *, params: Optional[Dict[str, Union[str, List[str]]]] = None) -> requests.Response:
-        return self._get(TwitchAPI.OFFICIAL_API + path, params=params, headers=self.headers)
+    def _helix_get(self, path: str, *, params: Optional[Dict] = None) -> Dict:
+        return self._request('get', urljoin(TwitchAPI.OFFICIAL_API, path), params=params).json()
+
+    def _helix_post(self, path: str, *, params: Optional[Dict] = None) -> requests.Response:
+        return self._request('post', urljoin(TwitchAPI.OFFICIAL_API, path), params=params)
+
+    def _wrap_requests(self, request_wrapper):
+        self._request = request_wrapper(self._request)
+        self._get = request_wrapper(self._request)
+
+
+class HubTopic(str):
+    @classmethod
+    def follows(cls, from_id: str = '', to_id: str = ''):
+        if not (from_id or to_id):
+            raise ValueError('Specify at least one argument from_id or to_id')
+        params = ''
+        params += f'from_id={from_id}' if from_id else ''
+        params += f'from_id={to_id}' if to_id else ''
+        return cls(urljoin(TwitchAPI.OFFICIAL_API, f'users/follows?{params}'))
+
+    @classmethod
+    def streams(cls, user_id: str):
+        return cls(urljoin(TwitchAPI.OFFICIAL_API, f'streams?user_id={user_id}'))
+
+
+class TwitchAPIError(Exception):
+    pass
+
+
+class RateLimitOverflow(TwitchAPIError):
+    pass
